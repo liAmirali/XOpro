b9385fca2e7a9b0b595a99f4301819721e452212
unknown <liamirali.lotfi@gmail.com>
2018-08-20 20:07:01 +0430

    New way to fit the height

--- www/js/setWandH.js
+++ www/js/setWandH.js
@@ -1,6 +1,9 @@
 const width = window.innerWidth;
 const height = window.innerHeight;
+setTimeout( function() {
+	const height_ = window.innerHeight;
+}, 2000);
 $('#wrapper').css({
 	'width': width + "px",
-	'height': height + 19 + "px"
+	'height': height + (height_ - height) + "px"
 });
