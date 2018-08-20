74d7fe5b0526f1ac277e640705909726eb2082b4
unknown <liamirali.lotfi@gmail.com>
2018-08-20 23:17:19 +0430

    Gave up with hiding Status Bar :)

--- www/js/setWandH.js
+++ www/js/setWandH.js
@@ -1,15 +1,6 @@
 const width = window.innerWidth;
 const height = window.innerHeight;
-$("#debugLog1").text(height);
 $('#wrapper').css({
 	'width': width + "px",
 	'height': height + "px"
 });
-setTimeout( function() {
-	const height_ = window.innerHeight;
-	$("#debugLog2").text(height_);
-	console.log("height_ is : " + height_);
-	$('#wrapper').css({
-		'height': height + (height_ - height) + "px"
-	});
-}, 2000);
