e75b3c8dfda41d649ae50334e65d7e42955eab39
unknown <liamirali.lotfi@gmail.com>
2018-08-20 22:45:34 +0430

    Debug for setting height fit to screen

--- www/js/setWandH.js
+++ www/js/setWandH.js
@@ -1,12 +1,13 @@
 const width = window.innerWidth;
 const height = window.innerHeight;
-console.log("height is : " + height);
+$("#debugLog1").text(height);
 $('#wrapper').css({
 	'width': width + "px",
 	'height': height + "px"
 });
 setTimeout( function() {
 	const height_ = window.innerHeight;
+	$("#debugLog2").text(height_);
 	console.log("height_ is : " + height_);
 	$('#wrapper').css({
 		'height': height + (height_ - height) + "px"
